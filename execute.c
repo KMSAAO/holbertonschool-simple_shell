c178a04b57108d377fcf08bfe279862cc3f22806
mido <midoxp@gmail.com>
2025-11-04 16:43:07 +0000

    Simple shell 0.3: add PATH resolution and skip fork if not found

--- execute.c
+++ execute.c
@@ -36,3 +36,139 @@ int execute_cmd(char **args, char *progname)
 
 	return (1);
 }
+#include "main.h"
+
+extern char **environ;
+
+/**
+ * get_path_value - returns the value of PATH from env
+ *
+ * Return: pointer to PATH value or NULL
+ */
+static char *get_path_value(void)
+{
+	int i;
+	size_t len;
+
+	if (!environ)
+		return (NULL);
+
+	for (i = 0; environ[i]; i++)
+	{
+		len = strlen("PATH=");
+		if (strncmp(environ[i], "PATH=", len) == 0)
+			return (environ[i] + len);
+	}
+	return (NULL);
+}
+
+/**
+ * find_in_path - search for command in PATH
+ * @cmd: command name (no slash)
+ *
+ * Return: malloc'ed full path on success, or NULL if not found
+ */
+static char *find_in_path(char *cmd)
+{
+	char *path, *path_copy, *dir, *full;
+	size_t len_dir, len_cmd;
+	struct stat st;
+
+	path = get_path_value();
+	if (!path)
+		return (NULL);
+
+	path_copy = strdup(path);
+	if (!path_copy)
+		return (NULL);
+
+	len_cmd = strlen(cmd);
+	dir = strtok(path_copy, ":");
+	while (dir)
+	{
+		len_dir = strlen(dir);
+		full = malloc(len_dir + 1 + len_cmd + 1);
+		if (!full)
+		{
+			free(path_copy);
+			return (NULL);
+		}
+		strcpy(full, dir);
+		full[len_dir] = '/';
+		strcpy(full + len_dir + 1, cmd);
+
+		if (stat(full, &st) == 0 && (st.st_mode & S_IXUSR))
+		{
+			free(path_copy);
+			return (full);
+		}
+		free(full);
+		dir = strtok(NULL, ":");
+	}
+	free(path_copy);
+	return (NULL);
+}
+
+/**
+ * execute_cmd - resolve command (absolute or from PATH) and exec
+ * @args: argument vector (command + args)
+ * @progname: shell name for error messages
+ *
+ * Return: 1 to continue loop
+ */
+int execute_cmd(char **args, char *progname)
+{
+	pid_t pid;
+	int status;
+	char *cmd_path = NULL;
+	struct stat st;
+
+	/* case 1: user typed a path like /bin/ls or ./a.out */
+	if (strchr(args[0], '/'))
+	{
+		if (stat(args[0], &st) == -1)
+		{
+			fprintf(stderr, "%s: not found\n", progname);
+			return (1);
+		}
+		cmd_path = args[0];
+	}
+	else
+	{
+		/* case 2: search in PATH */
+		cmd_path = find_in_path(args[0]);
+		if (!cmd_path)
+		{
+			fprintf(stderr, "%s: not found\n", progname);
+			return (1);
+		}
+	}
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror(progname);
+		if (cmd_path != args[0])
+			free(cmd_path);
+		return (1);
+	}
+
+	if (pid == 0)
+	{
+		if (execve(cmd_path, args, environ) == -1)
+		{
+			perror(progname);
+			if (cmd_path != args[0])
+				free(cmd_path);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+		if (cmd_path != args[0])
+			free(cmd_path);
+	}
+
+	return (1);
+}
